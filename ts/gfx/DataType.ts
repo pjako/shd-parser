63c656b210b4e012a7f7afe121d4a439f69995da
Peter Jakobs <p4jako@googlemail.com>
2016-11-21 00:24:08 +0100

    Added DataType

---
+++ ts/gfx/DataType.ts
@@ -0,0 +1,10 @@
+const GL = WebGLRenderingContext;
+export enum DataType {
+    Int8 = GL.BYTE,
+    Uint8 = GL.UNSIGNED_BYTE,
+    Int16 = GL.SHORT,
+    Uint16 = GL.UNSIGNED_SHORT,
+    Int32 = GL.INT,
+    Uint32 = GL.UNSIGNED_INT,
+    Float32 = GL.FLOAT,
+}
